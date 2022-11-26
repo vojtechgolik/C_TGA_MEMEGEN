41a4ee29e738b17223fbfbcef140da71a9b47dfb
Vojtech Golik <vojtechgolik@Vojtech-MacBook-Pro.local>
2022-11-26 23:29:17 +0100

    fixed memory leaks

--- main.c
+++ main.c
@@ -43,7 +43,11 @@ int bottom;
 // Loads an image from the given `path`.
 // If loading fails, return false.
 bool image_load(Image* image, const char* path) {
-    FILE* file = fopen(path, "rb");
+    FILE* file;
+    if(!(file = fopen(path, "rb"))){
+        return false;
+    }
+
 
     memset(image, 0, sizeof(Image));
     fread(&image->header, sizeof(image->header), 1, file);
@@ -74,7 +78,6 @@ void save_image(Image* image, const char* path){
 void write_text(const int posX, const int posY, Image* font_img, Image* to_img){
     for(int i = 0 ; i < font_img->canvas.width; i++){
         for(int j = 0 ; j < font_img->canvas.height; j ++){
-            //printf("Line: %d, row %d", j*(to_img->canvas.width), i);
             Pixel pixels = font_img->pixels[j*font_img->canvas.width + i];
             if(pixels.blue == 0 && pixels.green == 0 && pixels.red == 0){
                 continue;
@@ -95,7 +98,10 @@ void write(char* text, bool b, int pos, Font* font, Image* to){
             continue;
         }
         Image* image = &font->image[c-65];
-        //printf("Writing %c at pos: %d, %d\n", c, width+10, pos*(image -> canvas.height + vertical_spacing));
+        if(c == '\n' || c == '\0')
+        {
+            continue;
+        }
         if(b){
             if(bottom - 1 > pos){
                 write_text(width + 10, (to -> canvas.height - vertical_spacing - 25) - ((bottom-pos)*image->canvas.height), image, to);
@@ -103,6 +109,7 @@ void write(char* text, bool b, int pos, Font* font, Image* to){
                 write_text(width + 10, (to -> canvas.height - vertical_spacing - 25) - (pos*image->canvas.height), image, to);
             }
         }else{
+        
             write_text(width + 10, pos*(image -> canvas.height + vertical_spacing) + 10, image, to);
         }
         width += (image -> canvas.width);
@@ -121,9 +128,8 @@ bool load_fonts(Font* font, char* path){
     char* new_path = (char*) calloc(length + 7,1);
     strcat(new_path, path);
     strcat(new_path, "/X.tga");
-    printf("%s", new_path);
     for(int i = 0 ; i < 26; i ++){
-        new_path[length+1] = (starting_latter + i);//May cause issues ?
+        new_path[length+1] = (starting_latter + i);
         Image image = {};
         bool loaded = image_load(&image, new_path);
         if(loaded){
@@ -149,35 +155,38 @@ int main(int argc, char **argv){
     char* font_in = "font";
     if (argc < 4){
         printf("Wrong parameters\n");
-        image_in = "img1.tga";
-        font_in = "font";
-        image_out="meme.tga";
+        return 1;
     }else{
         image_in = argv[1];
         image_out = argv[2];
         font_in = argv[3];
     }
-    Font* font = (Font*)malloc(sizeof(font));
-    if (!load_fonts(font, font_in)){
+
+    Image image = {};
+    if (!image_load(&image, image_in)){
+        image_free(&image);
         printf("Could not load image\n");
         return 1;
     }
-    Image image = {};
-    image_load(&image, image_in);
+    Font* font = (Font*)malloc(sizeof(font));
+
+    if(!load_fonts(font, font_in)){
+        image_free(&image);
+        free_fonts(font);
+        printf("Could not load image\n");
+        return 1;
+    }
     char buffer[100] = {0};
     fgets(buffer, sizeof(buffer), stdin);
-    printf("\n params: %s \n", buffer);
     
     int top = atoi(&buffer[0]);
     bottom = atoi(&buffer[2]);
     for(int i = 0; i < top; i ++){
         fgets(buffer, sizeof(buffer), stdin);
-        printf("%s \n", buffer);
         write(buffer, false, i, font, &image);
     }
     for(int i = 0; i < bottom; i ++){
         fgets(buffer, sizeof(buffer), stdin);
-        printf("%s \n", buffer);
         write(buffer, true, i, font, &image);
     }
     save_image(&image, image_out);
